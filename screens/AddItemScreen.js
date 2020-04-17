dc7cc582157bcb122c1da45bea14d3c9fee77a8b
maja.evans <maja.evans@sap.com>
2020-04-17 10:17:54 -0700

    basic autocomplete search

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -1,27 +1,36 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
+import fireDb from '../firebasedb';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import { useStoreActions } from 'easy-peasy';
 
-function Item({ title, dashed }) {
+function Item({ uuid, price, dashed }) {
     return (
         <View style={{ ...styles.item, borderStyle: dashed ? 'dashed' : 'inherit' }}>
-            <Text style={styles.title}>{title}</Text>
+            <View style={{ flex: 1 }}>
+                <Text>{uuid}</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+                <Text style={{ textAlign: 'right' }}>{`$${price}`}</Text>
+            </View>
+
         </View>
     );
 }
 
 export default function AddItemScreen(props) {
+    const [name, setName] = useState('');
     const [item, setItem] = useState(null);
-    const { addItem } = useStoreActions(s => s.myOrder);
+    const [items, setItems] = useState([]);
 
-    const items = [
-        { title: 'Coca-Cola Soft Drinks, Regular, 355 mL, 24/CT' },
-        { title: 'Shoppers Drug Mart' },
-        { title: 'Whole Foods Market' },
-    ];
+    console.log(JSON.stringify(items));
+
+    useEffect(() => {
+        (async () => setItems(await fireDb.getCollection('grocery_items')))();
+    }, []);
+
+    const { addItem } = useStoreActions(s => s.myOrder);
 
     const onPress = () => {
         if (item) addItem(item);
@@ -31,12 +40,23 @@ export default function AddItemScreen(props) {
     return (
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
             <View style={styles.items}>
+                <Text style={{ fontWeight: 'bold' }}>Add Item</Text>
+                <TextInput
+                    placeholder="eggs"
+                    autoCapitalize="none"
+                    style={styles.search}
+                    onChangeText={async (text) => {
+                        setName(text);
+                        await setItems(await fireDb.getSuggestedGroceries(text));
+                    }}
+                    value={name}
+                />
                 <FlatList
                     data={items}
-                    renderItem={({ item }) => <Item title={item.title} />}
+                    renderItem={({ item }) => <Item {...item} dashed />}
                     keyExtractor={(item, i) => `${i}`}
                 />
-                <Button title="Add Item" onPress={onPress} dashed />
+                <Button title="Add Item" onPress={onPress} buttonStyle={styles.itemBtn} dashed />
             </View>
         </ScrollView>
     );
@@ -45,11 +65,11 @@ export default function AddItemScreen(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fafafa',
+        backgroundColor: '#F2F3FA'
     },
     contentContainer: {
         paddingTop: 15,
-        alignItems: 'center',
+        alignItems: 'center'
     },
     items: {
         flex: 1,
@@ -61,5 +81,21 @@ const styles = StyleSheet.create({
         borderRadius: 3,
         padding: 10,
         marginTop: 10,
+        flexDirection: 'row'
+    },
+    search: {
+        borderColor: '#2B3158',
+        height: 40,
+        borderRadius: 3,
+        borderWidth: 1,
+        marginTop: 10,
+        paddingLeft: 8
+    },
+    itemBtn: {
+        backgroundColor: '#F2F3FA',
+        borderColor: '#2B3158',
+        height: 50,
+        width: 180,
+        marginHorizontal: 10,
     },
 });
--- screens/PlaceOrder.js
+++ screens/PlaceOrder.js
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
     },
     overlay: {
         borderRadius: 3,
-        color: `#F2F3FA`
+        padding: 0
     },
     title: {},
 });
