2fcf099af189c49f1d77386026160cd5fa08663c
Alice Kim <alice.kim@concur.com>
2020-04-16 20:18:12 -0700

    implement ProfileScreen tab

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,13 +1,13 @@
-import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import * as React from 'react';
 
 import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
   return (
-    <Ionicons
+    <AntDesign
       name={props.name}
-      size={30}
+      size={20}
       style={{ marginBottom: -3 }}
       color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
     />
